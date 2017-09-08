e58092d79d58f7363e06b605a509da1aa23100d9
Rajdeep <rajdeep.sharma17@gmail.com>
2017-09-08 22:57:17 +0530

    7

--- src/assignment2/Pattern1.java
+++ src/assignment2/Pattern1.java
@@ -1,10 +1,22 @@
 package assignment2;
 
+import java.util.Scanner;
+
 public class Pattern1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scn = new Scanner(System.in);
+		int N = scn.nextInt();
+		int row,column,counter=0;
+		for(row=1;row<=N;row++){
+			for(column=1;column<=row;column++){
+				counter++;
+				System.out.print(counter);
+			}
+			System.out.println();
+		}
+		scn.close();
+		
 	}
 
 }
