0de315089565e37e54a2f930d29efa36b5e3d477
Rajdeep <rajdeep.sharma17@gmail.com>
2017-09-09 17:38:45 +0530

    Pascals-8

--- src/assignment2/Pattern3.java
+++ src/assignment2/Pattern3.java
@@ -3,23 +3,26 @@ package assignment2;
 import java.util.Scanner;
 
 public class Pattern3 {
-
+//nCk = (nC(k-1)*(n-k+1))/k  
+	//Pascals Triangle
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
 		int N = scn.nextInt();
-		int row;
-		System.out.println(1);
-		for(row = 2;row<=N;row++){
-			System.out.print(1);
-			if(row>=2){
-				System.out.print(row);
+		int row = 0;
+		while(row<N){
+			int counter = 1;
+			int col = 0;
+			while(col<=row){
+				System.out.print(counter + " ");
+				col+=1;
+				counter = (counter*(row - col +1))/col;
 			}
-			
-			
 			System.out.println();
+			row = row + 1;
 		}
+		
+		
 		scn.close();
-
-	}
-
+		}
 }
+
