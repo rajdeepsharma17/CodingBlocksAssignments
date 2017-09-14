f0810ef609c111f296a0a70bb9abe81182ec1831
Rajdeep <rajdeep.sharma17@gmail.com>
2017-09-14 22:13:27 +0530

    B2D

--- src/assignment3/B2D.java
+++ src/assignment3/B2D.java
@@ -1,10 +1,29 @@
 package assignment3;
 
+import java.util.Scanner;
+
 public class B2D {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		System.out.println("Enter binary no.");
+		int N = scn.nextInt();
+		int decimal = b2d(N);
+		System.out.println(decimal);
+		scn.close();
 
+	}
+	
+	public static int b2d(int N){
+		int dec = 0;
+		int counter = 1;
+		while(N!=0){
+			int rem = N%10;
+			dec = dec + rem*counter;
+			counter *= 2;
+			N=N/10;	
+		}
+		return dec;
 	}
 
 }
